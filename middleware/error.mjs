7430972c422ae352131783896f80b687d5cbb47e
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-11-04 22:21:16 +0530

    Add more use cases to error handler.

--- middleware/error.mjs
+++ middleware/error.mjs
@@ -4,12 +4,24 @@ export default function errorHandler(err, eq, res, next) {
   let error = { ...err }
 
   // Log to console for dev
-  console.log(err.stack.red)
+  console.log(err)
 
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`
     error = new ErrorResponse(message, 404)
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered'
+    error = new ErrorResponse(message, 400)
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message)
+    error = new ErrorResponse(message, 400)
   }
 
   res
