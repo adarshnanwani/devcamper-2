6519a41df8409720bf8ee8bdc0348edb0238e5ff
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-11-04 19:45:04 +0530

    Setup database connection.

--- server.mjs
+++ server.mjs
@@ -1,11 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
-// Route files
-import bootcamps from './routes/bootcamps.mjs'
+import connectDB from './config/db.mjs'
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
+
+// Route files
+import bootcamps from './routes/bootcamps.mjs'
+
+// Connect to database
+connectDB()
 
 const app = express()
 
@@ -17,8 +22,14 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `App listening in ${process.env.NODE_ENV} mode on port ${process.env.PORT}!`
   )
+})
+
+// Handle unhandled promise rejections
+process.on('unhandledRejections', (err) => {
+  console.log(`Error: ${err.message}`)
+  server.close(() => process.exit(1))
 })
